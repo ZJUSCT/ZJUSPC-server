142380ca4fa2e23267e91ca2e8e8705f48862124
xlnx <963504621@qq.com>
2017-09-28 18:32:42 +0800

    ok. Signed-off-by: xlnx <963504621@qq.com>

--- server.py
+++ server.py
@@ -1,63 +1,117 @@
 import sys
 import signal
 import ssl
+import time
 import json
 import struct
+from copy import deepcopy as cp
 import threading
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket, SimpleSSLWebSocketServer
 from optparse import OptionParser
 
 clients = []
 
 interval = 1
+port = 7194
+maxLen = 61
 
-import random
-test = {
-	"data": [
-		[67, 25, 49],
-		[100, 34, 86],
-		[1, 12, 21],
-	]
-}
+class DataGen:
+	def __init__(self):
+		self.data = [
+			[67, 25, 49, 12],
+			[100, 34, 86, 13],
+			[],
+			[1, 12, 21, 67],
+			[],
+			# if node not running then put a []
+		]
+	def getData(self):
+		import random
+		for i in range(0, len(self.data)):
+			for j in range(0, len(self.data[i])):
+				self.data[i][j] = (self.data[i][j] + random.randrange(-10, 10)) % 100
+		return cp(self.data)
+dataGen = DataGen()
 
-def getClusterStatus():
-	for i in range(0, len(test["data"])):
-		for j in range(0, len(test["data"][i])):
-			test["data"][i][j] += random.randrange(-5, 5)
-	return test["data"]
+def getNodes():
+	return ["cpu00", "cpu01", "cpu02", "gpu00", "mic00"]
+
+class Cluster:
+	def __init__(self):
+		# start the server!
+		self.nodeList = getNodes()
+		self.data = [[[[], ""] for x in range(0, maxLen)] for y in range(0, len(self.nodeList))]
+		# [
+		# 	[
+		# 		[[],], [], [], [], [] #maxLen (time)
+		# 	], #node0
+		# 	[
+		# 		[], [], [], [], []
+		# 	], #node1
+		# 	[
+		# 		[], [], [], [], []
+		# 	], #node2
+		# ]
+		# id for node, id.id for time
+		self.getStatus()
+
+	def getStatus(self):
+		self.currentStatus = dataGen.getData()
+		for i in range(0, len(self.nodeList)):
+			del self.data[i][0]
+			self.data[i].append([self.currentStatus[i], str(time.time())])
+		return self.currentStatus
+
+	def getInit(self):
+		return {
+			"head": self.nodeList,
+			"status": self.currentStatus
+		}
+
+cluster = Cluster()
 
 def dispatchData():
-	if len(clients) > 0:
-		data = getClusterStatus()
-		for client in clients:
-			client.sendMessage(json.dumps(data))
+	status = cluster.getStatus()
+	# keep fetching data for log and next connection
+	for client in clients:
+		client.sendMessage(json.dumps({
+			"basis": status
+		}))
 	t = threading.Timer(interval, dispatchData)
 	t.start()
 
 class ZJUSPCServer(WebSocket):
 	def handleMessage(self):
-		pass
+		self.data = json.loads(self.data)
+		msg = {
+			"extra": {}
+		} # sending extra package on server receive..
+		if "node" in self.data:
+			msg["extra"]["presentStatus"] = cluster.data[int(self.data["node"])]
+		if "viewState" in self.data:
+			pass
+		self.sendMessage(json.dumps(msg))
 		# for client in clients:
 		# 	if client != self:
 		# 		client.sendMessage(self.address[0] + u" - " + self.data)
 
 	def handleConnected(self):
-		print self.address, "connected"
+		print(self.address, "connected")
 		clients.append(self)
 		self.sendMessage(json.dumps({
-			"nodesInit": ["N1", "N2", "N3"],
+			"init": cluster.getInit(),
 		}))
 
 	def handleClose(self):
 		clients.remove(self)
-		print self.address, "closed"
+		print(self.address, "closed")
 	
 if __name__ == "__main__":
 	global server
 
 	parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
 	parser.add_option("--host", default="", type="string", action="store", dest="host", help="hostname (localhost)")
-	parser.add_option("--port", default=8000, type="int", action="store", dest="port", help="port (8000)")
+	parser.add_option("--port", default=port, type="int", action="store", dest="port", help="port (8000)")
 	parser.add_option("--ssl", default=0, type="int", action="store", dest="ssl", help="ssl (1: on, 0: off (default))")
 	parser.add_option("--cert", default="./cert.pem", type="string", action="store", dest="cert", help="cert (./cert.pem)")
 	parser.add_option("--key", default="./key.pem", type="string", action="store", dest="key", help="key (./key.pem)")
